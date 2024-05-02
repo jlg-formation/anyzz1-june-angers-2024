706946517770787af92a8192a28f1f2e016569a8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-05-02 19:44:29 +0200

    dev: shorter code for error message

--- front/src/app/services/error.service.ts
+++ front/src/app/services/error.service.ts
@@ -1,36 +1,32 @@
 import { Injectable } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, ValidationErrors } from '@angular/forms';
 import { Observable, map, of, startWith, tap } from 'rxjs';
+
+export const getMessage = (errors: ValidationErrors | null) => {
+  if (errors === null) {
+    return '';
+  }
+  if (errors?.['required']) {
+    return 'Champ requis';
+  }
+  if (errors?.['blackList']) {
+    return `Mot interdit (${errors?.['blackList'].blackListedWord})`;
+  }
+  return '';
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class ErrorService {
-  getMessage(f: FormGroup, fieldname: string) {
-    console.log('running getMessage on', fieldname);
-    const control = f.controls[fieldname];
-    if (!control) {
-      throw new Error('control does not exist');
-    }
-    if (control.valid) {
-      return '';
-    }
-    if (control.errors?.['required']) {
-      return 'Champ requis';
-    }
-    if (control.errors?.['blackList']) {
-      return `Mot interdit (${control.errors?.['blackList'].blackListedWord})`;
-    }
-    return '';
-  }
-
   getErrors(f: FormGroup, fieldname: string): Observable<string> {
     console.log('getErrors');
-    return f.controls[fieldname].valueChanges.pipe(
-      startWith(f.controls[fieldname].value),
+    const control = f.controls[fieldname];
+    return control.valueChanges.pipe(
+      startWith(control.value),
       map((value) => {
         console.log('value: ', value);
-        return this.getMessage(f, fieldname);
+        return getMessage(control.errors);
       })
     );
   }
