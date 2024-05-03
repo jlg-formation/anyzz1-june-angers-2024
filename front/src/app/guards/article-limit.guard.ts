02892aea266971fb97efd0202378c6b8acc99c99
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-05-03 15:44:02 +0200

    dev: added guards

--- front/src/app/guards/article-limit.guard.ts
+++ front/src/app/guards/article-limit.guard.ts
@@ -1,6 +1,30 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import {
+  CanActivateFn,
+  UrlTree,
+  createUrlTreeFromSnapshot,
+} from '@angular/router';
+import { ArticleService } from '../services/article.service';
+import { of, switchMap } from 'rxjs';
 
 export const articleLimitGuard: CanActivateFn = (route, state) => {
   console.log('articleLimitGuard');
-  return true;
+  const articleService = inject(ArticleService);
+
+  return of(undefined).pipe(
+    switchMap(async () => {
+      if (articleService.articles() === undefined) {
+        await articleService.load();
+      }
+      const articles = articleService.articles();
+      if (articles === undefined) {
+        return true;
+      }
+      if (articles.length < 5) {
+        return true;
+      }
+      window.alert("Trop d'articles. Lisez les mentions légales... ");
+      return createUrlTreeFromSnapshot(route, ['/legal']);
+    })
+  );
 };
