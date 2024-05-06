31784ca124a52c2b83969e02a46030fd1faf5cc2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-05-06 19:48:04 +0200

    dev: added ssr
    command:
    
    npx ng add @angular/ssr

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -4,6 +4,7 @@ import { TitleStrategy, provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 import { TitleService } from './services/title.service';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +13,6 @@ export const appConfig: ApplicationConfig = {
     {
       provide: TitleStrategy,
       useClass: TitleService,
-    },
+    }, provideClientHydration(),
   ],
 };
