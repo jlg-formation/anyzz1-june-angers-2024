eb2a575d819413532e25b8633ba65f418e35364f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-05-08 13:11:58 +0200

    dev: test with karma
    Level 1/4 : all tests should compiled

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,16 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +15,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'front'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('front');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('front app is running!');
+    expect(compiled.querySelector('header span')?.textContent).toContain(
+      'Gestion Stock'
+    );
   });
 });
--- front/src/app/routes/legal/legal.component.spec.ts
+++ front/src/app/routes/legal/legal.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LegalComponent } from './legal.component';
+import LegalComponent from './legal.component';
 
 describe('LegalComponent', () => {
   let component: LegalComponent;
@@ -8,9 +8,8 @@ describe('LegalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LegalComponent ]
-    })
-    .compileComponents();
+      declarations: [LegalComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
