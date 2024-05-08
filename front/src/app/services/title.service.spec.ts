fdeafe61fb7835824302fb0300f830f71db50abe
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-05-08 16:44:51 +0200

    dev: added coverage on title service

--- front/src/app/services/title.service.spec.ts
+++ front/src/app/services/title.service.spec.ts
@@ -1,16 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 
+import { Router, provideRouter } from '@angular/router';
+import { routes } from '../app.routes';
 import { TitleService } from './title.service';
 
 describe('TitleService', () => {
   let service: TitleService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideRouter(routes)],
+    });
     service = TestBed.inject(TitleService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should be Gestion Stock for home page', () => {
+    const router = TestBed.inject(Router);
+    service.updateTitle(router.routerState.snapshot);
+    expect(document.title).toEqual('Gestion Stock');
+  });
+
+  it('should be Gestion Stock for legal page', (done) => {
+    const router = TestBed.inject(Router);
+    router.navigateByUrl('/legal').then(() => {
+      service.updateTitle(router.routerState.snapshot);
+      expect(document.title).toEqual('Gestion Stock - Mentions Légales');
+      done();
+    });
   });
 });
