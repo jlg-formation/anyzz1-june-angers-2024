a21f9eb1256a9b60e566c47571d8bd8c1df665d9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-05-08 12:55:34 +0200

    dev: testing the library
    do not forget to:
    declare peerDependancies

--- test-widgets/src/app/app.component.ts
+++ test-widgets/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { AsyncBtnComponent, ClockComponent } from '@gestionstock/widgets';
+import { Observable, delay, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncBtnComponent, ClockComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'test-widgets';
+  faHome = faHome;
+
+  doSomething(): Observable<void> {
+    return of(undefined).pipe(delay(2000));
+  }
 }
