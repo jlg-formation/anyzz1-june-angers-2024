eed67a1fa42f9abf97f8c3e92d801a63dd1a71d4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-05-09 13:34:51 +0200

    dev: added filter logic and added it in config

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -10,11 +10,15 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes';
 import { TitleService } from './services/title.service';
 import { authInterceptor } from './interceptors/auth.interceptor';
+import { filterInterceptor } from './interceptors/filter.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([authInterceptor, filterInterceptor])
+    ),
     provideClientHydration(),
     {
       provide: TitleStrategy,
--- front/src/app/interceptors/filter.interceptor.ts
+++ front/src/app/interceptors/filter.interceptor.ts
@@ -1,5 +1,25 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpResponse } from '@angular/common/http';
+import { map } from 'rxjs';
 
 export const filterInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  return next(req).pipe(
+    map((httpEvent) => {
+      if (!(httpEvent instanceof HttpResponse)) {
+        return httpEvent;
+      }
+      const response = httpEvent;
+
+      if (response.body instanceof Array) {
+        const body = response.body.filter((item) => {
+          const str = JSON.stringify(item);
+          if (str.includes('hidden')) {
+            return false;
+          }
+          return true;
+        });
+        return response.clone({ body });
+      }
+      return response;
+    })
+  );
 };
