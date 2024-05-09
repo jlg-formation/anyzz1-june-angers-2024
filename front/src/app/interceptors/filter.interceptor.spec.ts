16595ccd3af4ffdc1d304a0df0de6cfce7dfc6f9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-05-09 13:47:20 +0200

    dev: added unit test for filter interceptor

--- front/src/app/interceptors/filter.interceptor.spec.ts
+++ front/src/app/interceptors/filter.interceptor.spec.ts
@@ -1,10 +1,17 @@
+import {
+  HttpEventType,
+  HttpHandlerFn,
+  HttpInterceptorFn,
+  HttpRequest,
+  HttpResponse,
+} from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
 
+import { of } from 'rxjs';
 import { filterInterceptor } from './filter.interceptor';
 
-describe('filterInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) => 
+fdescribe('filterInterceptor', () => {
+  const interceptor: HttpInterceptorFn = (req, next) =>
     TestBed.runInInjectionContext(() => filterInterceptor(req, next));
 
   beforeEach(() => {
@@ -13,5 +20,49 @@ describe('filterInterceptor', () => {
 
   it('should be created', () => {
     expect(interceptor).toBeTruthy();
+  });
+
+  it('should be filter one article', (done) => {
+    const next: HttpHandlerFn = () => {
+      return of(
+        new HttpResponse({ body: [{ name: 'hidden' }, { name: 'regular' }] })
+      );
+    };
+    const req = new HttpRequest('GET', '/truc');
+    const obs = interceptor(req, next);
+    obs.subscribe((httpEvent) => {
+      const response = httpEvent as HttpResponse<unknown>;
+      expect(response.body).toEqual([{ name: 'regular' }]);
+      done();
+    });
+  });
+
+  it('should be do nothing when not an array', (done) => {
+    const next: HttpHandlerFn = () => {
+      return of(new HttpResponse({ body: 'not an array' }));
+    };
+    const req = new HttpRequest('GET', '/truc');
+    const obs = interceptor(req, next);
+    obs.subscribe((httpEvent) => {
+      const response = httpEvent as HttpResponse<unknown>;
+      expect(response.body).toEqual('not an array');
+      done();
+    });
+  });
+
+  it('should be do nothing when not an http response', (done) => {
+    const next: HttpHandlerFn = () => {
+      return of({
+        type: HttpEventType.Sent,
+      });
+    };
+    const req = new HttpRequest('GET', '/truc');
+    const obs = interceptor(req, next);
+    obs.subscribe((httpEvent) => {
+      expect(httpEvent).toEqual({
+        type: HttpEventType.Sent,
+      });
+      done();
+    });
   });
 });
