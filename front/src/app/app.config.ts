de101d429edb0718bf1cf6aa5a9b888926c327ff
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-05-09 12:52:36 +0200

    dev: added the interceptor in the config

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,15 +1,20 @@
 import { ApplicationConfig } from '@angular/core';
 import { TitleStrategy, provideRouter } from '@angular/router';
 
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes';
 import { TitleService } from './services/title.service';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
     provideClientHydration(),
     {
       provide: TitleStrategy,
--- front/src/app/interceptors/auth.interceptor.ts
+++ front/src/app/interceptors/auth.interceptor.ts
@@ -1,5 +1,6 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  console.log('req: ', req);
   return next(req);
 };
