4949d90fd3445e06c1945df5517a721650e870af
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-05-09 13:17:33 +0200

    dev: added unit test for interceptor auth

--- front/src/app/interceptors/auth.interceptor.spec.ts
+++ front/src/app/interceptors/auth.interceptor.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
+import {
+  HttpHandlerFn,
+  HttpInterceptorFn,
+  HttpRequest,
+  HttpResponse,
+} from '@angular/common/http';
 
 import { authInterceptor } from './auth.interceptor';
+import { of, tap } from 'rxjs';
 
 describe('authInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) => 
+  const interceptor: HttpInterceptorFn = (req, next) =>
     TestBed.runInInjectionContext(() => authInterceptor(req, next));
 
   beforeEach(() => {
@@ -13,5 +19,24 @@ describe('authInterceptor', () => {
 
   it('should be created', () => {
     expect(interceptor).toBeTruthy();
+  });
+
+  it('should be authorizing', (done) => {
+    let expectedReq: HttpRequest<unknown> | undefined = undefined;
+    const next: HttpHandlerFn = (req: HttpRequest<unknown>) => {
+      return of(new HttpResponse()).pipe(
+        tap(() => {
+          expectedReq = req;
+        })
+      );
+    };
+    const req = new HttpRequest('GET', '/truc');
+    const result = interceptor(req, next);
+    result.subscribe(() => {
+      expect(expectedReq?.headers.get('Authorization')).toEqual(
+        'Bearer this is my auth token'
+      );
+      done();
+    });
   });
 });
