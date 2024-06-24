63245282cfbbeaf033d0226c444f2595c7d2d00b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-24 14:52:46 +0200

    dev: reorganize import

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
-import * as eee from './routes/legal/legal.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
