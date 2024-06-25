0849f12617d9e379860f7b5707c05922e76483b0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-25 11:24:32 +0200

    dev: observable

--- examples/observable/01-observable.js
+++ examples/observable/01-observable.js
@@ -1,1 +1,34 @@
 const { Observable } = require("rxjs");
+
+const obs = new Observable((subscriber) => {
+  console.log("je demarre");
+  subscriber.next(123);
+  subscriber.next("coucou");
+  const timer = setTimeout(() => {
+    subscriber.next(false);
+    subscriber.complete();
+    console.log("je finis");
+  }, 1000);
+
+  return () => {
+    console.log("fin de vie");
+    clearTimeout(timer);
+  };
+});
+
+const observer = {
+  next: (data) => {
+    console.log("data: ", data);
+  },
+  error: (err) => {
+    console.log("err: ", err);
+  },
+  complete: () => {
+    console.log("complete");
+  },
+};
+
+const s = obs.subscribe(observer);
+setTimeout(() => {
+  s.unsubscribe();
+}, 500);
