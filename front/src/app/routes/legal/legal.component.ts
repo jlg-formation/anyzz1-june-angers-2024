c6180ac88479c89ba1d869646094ff149bd59840
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-25 15:29:23 +0200

    dev: clock

--- front/src/app/routes/legal/legal.component.ts
+++ front/src/app/routes/legal/legal.component.ts
@@ -1,10 +1,19 @@
+import { AsyncPipe, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { Observable, interval, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-legal',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, DatePipe],
   templateUrl: './legal.component.html',
   styleUrls: ['./legal.component.scss'],
 })
-export default class LegalComponent {}
+export default class LegalComponent {
+  clock$: Observable<Date> = interval(1000).pipe(
+    map((x) => new Date()),
+    tap((x) => {
+      console.log('x: ', x);
+    }),
+  );
+}
