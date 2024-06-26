e7bda1ea128c4daa91ffb89ff19057b43778de71
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-26 16:44:28 +0200

    dev: added titles

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, title: 'Gestion Stock - Accueil' },
   {
     path: 'legal',
     loadComponent: () => import('./routes/legal/legal.component'),
+    title: 'Gestion Stock - Mentions Légales',
   },
   {
     path: 'stock',
     loadChildren: () => import('./stock/stock.routes'),
+    title: 'Gestion Stock - Stock',
   },
 ];
--- front/src/app/stock/stock.routes.ts
+++ front/src/app/stock/stock.routes.ts
@@ -4,10 +4,12 @@ const stockRoutes: Routes = [
   {
     path: '',
     loadComponent: () => import('./list/list.component'),
+    title: 'Gestion Stock - Liste des articles',
   },
   {
     path: 'create',
     loadComponent: () => import('./create/create.component'),
+    title: 'Gestion Stock - Ajoute un article',
   },
 ];
 
