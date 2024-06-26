b9e85dd88750eb6b7bbad9b8ff8de7e2e4448361
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-26 17:09:58 +0200

    dev: no console log

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { isDevMode } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const noop = () => {
+  // hello eslint
+};
+
+if (!isDevMode()) {
+  console.log = noop;
+  console.warn = noop;
+  console.error = noop;
+}
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err),
+);
